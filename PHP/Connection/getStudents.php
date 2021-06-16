795b125b3bbfcb5e584ba654b82c332a0977e925
Briones Dos Santos Gabriel Dario <dossantosgabrielb@gmail.com>
2021-06-15 21:17:20 -0300

    Fixed * for column names

--- PHP/Connection/getStudents.php
+++ PHP/Connection/getStudents.php
@@ -2,7 +2,7 @@
 include_once "./connection.php";
 
 // Sentencia SQL para obtener los datos de la tabla.
-$sql = $pdo->prepare("SELECT * FROM alumnos");
+$sql = $pdo->prepare("SELECT ID, nombre, apellido, edad, email, escuela FROM alumnos");
 $sql->execute();
 $students = $sql->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($students);
